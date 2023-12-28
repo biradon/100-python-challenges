5fd7af85ee42cbc67bb7e2d7889dc1813eec65bf
Minh Truong <61044654+biradon@users.noreply.github.com>
2023-12-27 22:50:25 -0500

    reupload

--- 10.py
+++ 10.py
@@ -1,41 +1,28 @@
-# from itertools import combinations
-
-# def calc_max_possible_change(values):
-#     final_results = 0
-#     max_sum = sum(values)
-#     results = [False] * max_sum
-
-#     # Include individual values
-#     for val in values:
-#         results[val - 1] = True
-
-#     # Include combinations of values
-#     for r in range(2, len(values) + 1):
-#         for combo in combinations(values, r):
-#             results[sum(combo) - 1] = True
-
-#     for i in range(0, len(results)):
-#         if results[i]:
-#             final_results+=1
-#         else:
-#             break
-
-#     return final_results
-
-
-
-
+from itertools import combinations
 
 def calc_max_possible_change(values):
-    # wrappng / copying necessary so that we do not sort the original
-    sorted_numbers = list(values)
-    sorted_numbers.sort()
-    max_possible_change = 0
-    for current_value in sorted_numbers:
-        if current_value > max_possible_change + 1:
+    final_results = 0
+    max_sum = sum(values)
+    results = [False] * max_sum
+
+    # Include individual values
+    for val in values:
+        results[val - 1] = True
+
+    # Include combinations of values
+    for r in range(2, len(values) + 1):
+        for combo in combinations(values, r):
+            results[sum(combo) - 1] = True
+
+    for i in range(0, len(results)):
+        if results[i]:
+            final_results+=1
+        else:
             break
-        max_possible_change += current_value
-    return max_possible_change
+
+    return final_results
+
+
 
 array = [1, 1, 1]
 print(calc_max_possible_change(array))
